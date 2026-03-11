e15ab1a75d36dfcf8c819cce017cddfb3ad8ea26
cshar21 <chakshusharma21s@gmail.com>
2026-03-11 14:22:10 +0530

    Base application setup

---
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Palindrome Checker ");
+        System.out.println(" Application Version: 1.0");
+        System.out.println("=================================");
+
+    }
+}
